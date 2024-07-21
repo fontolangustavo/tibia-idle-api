2358713c6b61a93def8b9750c1de620a08985ced
Fontolan <fontolangustavo@gmail.com>
2024-07-21 18:18:37 -0300

    chore: init files

---
+++ src/test/java/com/fontolan/tibiaidle/TibiaidleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fontolan.tibiaidle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TibiaidleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
